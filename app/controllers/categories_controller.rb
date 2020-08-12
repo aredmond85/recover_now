58f673706944f0137c0d6512bc0b6970020e9086
aredmond85 <aredmond85@gmail.com>
2020-08-11 21:32:41 -0500

    updated routes

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,46 @@
+class CategoriesController < ApplicationController
+    def index
+        @category = Category.all
+    end
+
+    def show
+        @category = Category.find(params[:id])
+    end
+    
+    def new
+        @category = Category.new
+    end
+
+    def create
+        @category = Category.new(category_params)
+        @category.save
+        redirect_to report_path
+    end
+
+    def update
+        @category = Category.find(params[:id])
+        @category.update(category_params)
+        redirect_to @category
+    end
+
+    def edit
+        @category = Category.find(params[:id])
+    end
+
+    def destroy
+        @category = Category.find(params[:id])
+        @category.destroy
+        redirect_to report_path
+    end
+
+    private
+
+    def set_category
+        @category = Category.find(params[:id])
+    end
+
+        def category_params
+            params.require(:category).permit(:name)
+        end
+
+end
