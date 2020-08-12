4c577199b73bc98e3cf6de25d3820cdda0731373
aredmond85 <aredmond85@gmail.com>
2020-08-11 19:13:45 -0500

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  resources :pages
+  resources :categories
+  resources :reports
+  resources :responders
+  resources :users
+  root 'pages#home'
+  get 'about', to: 'pages#about'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
