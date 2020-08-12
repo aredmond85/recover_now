9cb522c9462bdfcec6276964ba023226d807cc67
aredmond85 <aredmond85@gmail.com>
2020-08-11 19:53:55 -0500

    added reports index table

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,10 @@ Rails.application.routes.draw do
   resources :reports
   resources :responders
   resources :users
-  root 'pages#home'
   get 'about', to: 'pages#about'
+
+  root 'pages#home'
+ 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
