92c7e056506f6a5e37908ab73b64970dba5a5190
aredmond85 <aredmond85@gmail.com>
2020-08-11 20:01:01 -0500

    added index pages and fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,9 @@
 Rails.application.routes.draw do
-  resources :pages
-  resources :categories
-  resources :reports
-  resources :responders
-  resources :users
+  resources :category
+  resources :report
+  resources :responder
+  resources :user
   get 'about', to: 'pages#about'
 
   root 'pages#home'
- 
-
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
