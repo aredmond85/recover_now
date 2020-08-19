3f682b96aec464b5e34849432f5faa53c8716304
aredmond85 <aredmond85@gmail.com>
2020-08-18 21:27:47 -0500

    fixed collection_select and inputs to db

--- db/migrate/20200818222532_create_report_heros.rb
+++ db/migrate/20200818222532_create_report_heros.rb
@@ -1,10 +1,8 @@
 class CreateReportHeros < ActiveRecord::Migration[6.0]
   def change
-    create_table :report_heros, id: false do |t|
-      t.references :report, index: false, foreign_key: true
-      t.references :hero, index: false, foreign_key: true
-
-      t.timestamps
+    create_table :report_heros do |t|
+      t.integer :report_id
+      t.integer :hero_id
     end
   end
 end
