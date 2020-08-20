549cf5404e908a68a0317302519b3ca77aa4a681
aredmond85 <aredmond85@gmail.com>
2020-08-19 23:44:47 -0500

    corrected schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_18_222532) do
+ActiveRecord::Schema.define(version: 2020_08_19_220628) do
 
   create_table "heros", force: :cascade do |t|
     t.string "hero_name"
@@ -23,6 +23,11 @@ ActiveRecord::Schema.define(version: 2020_08_18_222532) do
     t.integer "hero_id"
   end
 
+  create_table "report_villains", force: :cascade do |t|
+    t.integer "report_id"
+    t.integer "villain_id"
+  end
+
   create_table "reports", force: :cascade do |t|
     t.string "subject"
     t.text "description"
@@ -30,6 +35,7 @@ ActiveRecord::Schema.define(version: 2020_08_18_222532) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
     t.integer "hero_id"
+    t.integer "villain_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -42,6 +48,12 @@ ActiveRecord::Schema.define(version: 2020_08_18_222532) do
     t.datetime "remember_created_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "villains", force: :cascade do |t|
+    t.string "villain_name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
