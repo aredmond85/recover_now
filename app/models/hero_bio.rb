b29a50bdc1bc245e3f43ff398175b3bab81f92d0
aredmond85 <aredmond85@gmail.com>
2020-08-20 21:14:29 -0500

    added validations to bios

--- app/models/hero_bio.rb
+++ app/models/hero_bio.rb
@@ -1,3 +1,6 @@
 class HeroBio < ApplicationRecord
+    validates :hero_summary, presence: true, length: { minimum: 100, maximum: 500 }
+    validates :hero_powers, presence: true, length: { minimum: 5, maximum: 500 }
+
     belongs_to :hero
 end
--- app/models/villain_bio.rb
+++ app/models/villain_bio.rb
@@ -1,3 +1,6 @@
 class VillainBio < ApplicationRecord
+    validates :villain_summary, presence: true, length: { minimum: 100, maximum: 500 }
+    validates :villain_powers, presence: true, length: { minimum: 5, maximum: 500 }
+    
     belongs_to :villain
 end
