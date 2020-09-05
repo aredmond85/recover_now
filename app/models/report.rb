186d56d48701fb74e1d36e50f558c4620aecc65e
aredmond85 <aredmond85@gmail.com>
2020-09-05 09:49:16 -0500

    updated search function

--- app/models/report.rb
+++ app/models/report.rb
@@ -10,17 +10,26 @@ class Report < ApplicationRecord
    has_many :report_villains
    has_many :villains, through: :report_villains
 
-    def self.search(search)
-        if search
-            hero = Hero.find_by(hero_name: search)
-                if hero
-                    self.where(hero_id: hero)
-                else
-                    Report.all
-                end
-        else
-            Report.all
-        end
-    end
+    # def self.search(search)
+    #     if search
+    #         hero = Hero.find_by(hero_name: search)
+    #             if hero
+    #                 self.where(hero_id: hero.id)
+    #             else
+    #                 Report.all
+    #             end
+    #     else
+    #         Report.all
+    #     end
+    # end
     
+    def self.search(name)
+        name = name.to_s.strip
+        
+        if name.present?
+          reports = Report.joins(:heros).where(heros: { hero_name: name }).presence 
+        end
+    
+        reports ||= Report.all
+      end
 end
